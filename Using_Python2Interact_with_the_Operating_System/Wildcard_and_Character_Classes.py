fb21b01b0e7909f016f46b240f15432cece92bf5
Niloy Ranjan Dev Tirtha <nrdev10@gmail.com>
2020-08-02 00:56:52 +0600

    Create Wildcard_and_Character_Classes.py

---
+++ Using_Python2Interact_with_the_Operating_System/Wildcard_and_Character_Classes.py
@@ -0,0 +1,11 @@
+''' Return true if the vowels a, e and i are seen sequenciallly with a consonent between them'''
+
+import re
+def check_aei (text):
+    pattern = (r"[^aei]?[aei]?[^aei]+[aei]?[^aei]")
+    result = re.search(pattern, text)
+    return result != None
+
+print(check_aei("academia")) # True
+print(check_aei("aerial")) # False
+print(check_aei("paramedic")) # True

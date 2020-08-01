ea5409ba7863340fbb76da324a98a67093597bcd
Niloy Ranjan Dev Tirtha <nrdev10@gmail.com>
2020-08-02 01:03:53 +0600

    Update Repetition_Qualifiers.py

--- Using_Python2Interact_with_the_Operating_System/Repetition_Qualifiers.py
+++ Using_Python2Interact_with_the_Operating_System/Repetition_Qualifiers.py
@@ -1,10 +1,11 @@
 ''' The repeating_letter_a function checks if the text passed includes the letter "a" (lowercase or uppercase) at least twice.
-  For example, repeating_letter_a("banana") is True, while repeating_letter_a("pineapple") is False. 
+  For example, repeating_letter_a("banana") is True, while repeating_letter_a("pineapple") is False.
   Fill in the code to make this work. '''
-  
+
 import re
 def repeating_letter_a(text):
-    result = re.search(r"a+\w*a+", text, re.IGNORECASE)
+    pattern = re.compile(r"a+.*a+")
+    result = re.search(pattern, text.lower())
     return result != None
 
 print(repeating_letter_a("banana")) # True

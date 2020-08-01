3ed56132c27b538ae61983b125724f66399287ee
Niloy Ranjan Dev Tirtha <nrdev10@gmail.com>
2020-08-02 04:30:36 +0600

    Update Capturing _Groups.py

--- Using_Python2Interact_with_the_Operating_System/Capturing _Groups.py
+++ Using_Python2Interact_with_the_Operating_System/Capturing _Groups.py
@@ -3,10 +3,10 @@
 import re
 def rearrange_name(name):
     result = re.search(r"^(\w*), (\w*) (\w?\.)$", name)
-    print(result)
+    #print(result)
     if result == None:
         return name
-    return "{} {} {}".format(result[3], result[2], result[1])
+    return "{} {} {}".format(result[2], result[3], result[1])
 
 name=rearrange_name("Kennedy, John F.")
 print(name)
